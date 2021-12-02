368e5024104a44533e781a820a5b8f6c7ca2d90d
filipemmacedo <filipemacedo55@gmail.com>
2021-12-02 23:33:33 +0000

    added new pages from newspapper

--- index.js
+++ index.js
@@ -2,31 +2,81 @@ const PORT = 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
-const { contains } = require('cheerio/lib/static')
-
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json('Bem vindos à minha API')
-})
+const newspapers = [
+    {
+        name: 'jornal-de-noticias',
+        address: 'https://www.jn.pt/inovacao.html',
+        base: 'https://www.jn.pt/inovacao.html/'
+    },
+    {
+        name: 'jornal-de-negocios',
+        address: 'https://www.jornaldenegocios.pt/empresas/tecnologias',
+        base: 'https://www.jornaldenegocios.pt/'
+    },
+    {
+        name: 'publico',
+        address: 'https://www.publico.pt/tecnologia',
+        base: ''
+    },
+]
 
-app.get('/news', (req, res) => {
-    axios.get('https://www.jornaldenegocios.pt/mercados')
-        .then((response) => {
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
             const html = response.data
             const $ = cheerio.load(html)
-            const articles = []
 
-        $('a:contains("bolsas")', html).each(function () {
-            const title = $(this).text()
-            const url = $(this).attr('href')
-            articles.push({
-                title,
-                url,
+            $('a[href*="tecnologia"]', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
             })
-            }) 
-            res.json(articles)
+
         })
 })
 
-app.listen(PORT, () => console.log(`Working on Port ${PORT}`))
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a[href*="tecnologia"]', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
